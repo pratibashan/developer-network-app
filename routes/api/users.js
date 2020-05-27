998ab5be88d7fa7a911fd3658182a2427c6f6768
Pratiba Shan <pratibashan.g@gmail.com>
2020-05-27 16:38:13 -0500

    create update delete profile done

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,82 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
 
-//@route   GET api/users
-//@desc    Test route
+//async await can be used in Asynchronous fn whenever we get a promise from the fn call. We can avoid using .then & response lines since async/await waits until it gets the response.
+
+//@route   POST api/users
+//@desc    Register user
 //@access  Public
 
-router.get('/', (req, res) => res.send('User Route'));
+router.post('/', [
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Please include a valid Email').isEmail(),
+  check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+], async (req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { name, email, password } = req.body;
+
+  try {
+    //See if user exists in DB
+    let user = await User.findOne({ email }) // findOne({email:email})
+    if (user) {
+      return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+    }
+
+    //Get users gravator
+    const avatar = gravatar.url(email, {
+      s: '200',
+      r: 'pg',
+      d: 'mm'
+    })
+
+    //creating a new user object
+    user = new User({
+      name,
+      email,
+      password,
+      avatar
+    })
+
+    // Encrypt Password
+    // need to first create a salt before we hash the password
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    //saving the new user in the DB
+    await user.save();
+
+    //Return jsonwebtoken
+    const payload = {
+      user: {
+        id: user.id // id from DB
+      }
+    }
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      })
+  }
+  catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
+
+});
 
 
 module.exports = router;
